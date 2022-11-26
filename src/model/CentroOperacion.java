f4cbd1e46ff3d6b28c86994aa7406081901770be
Sebas-gifPaz777 <pazsebas797@gmail.com>
2022-11-26 10:16:50 -0500

    Last version restored

--- src/model/CentroOperacion.java
+++ src/model/CentroOperacion.java
@@ -3,6 +3,7 @@ package model;
 import grafo.ListaAdyacencia;
 import grafo.MatrizAdyacencia;
 import grafo.Vertice;
+import enums.Color;
 
 public class CentroOperacion {
 	
@@ -11,9 +12,15 @@ public class CentroOperacion {
 	private ListaAdyacencia<String> listaEstacionesDistancia;
 	private MatrizAdyacencia<String> matrizEstacionesDistancia;
 	
+	private ListaAdyacencia<String> listaEstacionesTiempo;
+	private MatrizAdyacencia<String> matrizEstacionesTiempo;
+	
 	public CentroOperacion() {
 		this.listaEstacionesDistancia = new ListaAdyacencia<>(true);
 		this.matrizEstacionesDistancia = new MatrizAdyacencia<>(NUMERO_VERTICES);
+		
+		this.listaEstacionesTiempo = new ListaAdyacencia<>(true);
+		this.matrizEstacionesTiempo = new MatrizAdyacencia<>(NUMERO_VERTICES);
 	}
 	
 	public ListaAdyacencia<String> getListaEstacionesDistancia() {
@@ -24,46 +31,107 @@ public class CentroOperacion {
 		return this.matrizEstacionesDistancia;
 	}
 	
+	public ListaAdyacencia<String> getListaEstacionesTiempo() {
+		return listaEstacionesTiempo;
+	}
+
+	public MatrizAdyacencia<String> getMatrizEstacionesTiempo() {
+		return matrizEstacionesTiempo;
+	}
+
 	public void agregarEstacion(String nombreEstacion) {
 		listaEstacionesDistancia.agregarVertice(nombreEstacion);
+		listaEstacionesTiempo.agregarVertice(nombreEstacion);
 		matrizEstacionesDistancia.agregarVertice(nombreEstacion);
+		matrizEstacionesTiempo.agregarVertice(nombreEstacion);
 	}
 	
 	public boolean eliminarEstacionLED(String nombreEstacion) {
 		return listaEstacionesDistancia.eliminarVertice(nombreEstacion);
 	}
 	
+	public boolean eliminarEstacionLET(String nombreEstacion) {
+		return listaEstacionesTiempo.eliminarVertice(nombreEstacion);
+	}
+	
 	public boolean eliminarEstacionMED(String nombreEstacion) {
 		return matrizEstacionesDistancia.eliminarVertice(nombreEstacion);
+	}
+	
+	public boolean eliminarEstacionMET(String nombreEstacion) {
+		return matrizEstacionesTiempo.eliminarVertice(nombreEstacion);
 	}
 	
 	public Vertice<String> buscarEstacionLED(String nombreEstacion) {
 		return listaEstacionesDistancia.buscarVertice(nombreEstacion);
 	}
 	
+	public Vertice<String> buscarEstacionLET(String nombreEstacion) {
+		return listaEstacionesTiempo.buscarVertice(nombreEstacion);
+	}
+	
 	public Vertice<String> buscarEstacionMED(String nombreEstacion) {
 		return matrizEstacionesDistancia.buscarVertice(nombreEstacion);
 	}
 	
+	public Vertice<String> buscarEstacionMET(String nombreEstacion) {
+		return matrizEstacionesTiempo.buscarVertice(nombreEstacion);
+	}
+	
 	public boolean agregarRutaDistancia(String origen, String destino, int distancia) {
-		boolean a, b;
-		a = matrizEstacionesDistancia.agregarAristaMatriz(origen, destino, distancia);
-		b = listaEstacionesDistancia.agregarArista(origen, destino, distancia);
-		if ( a && b ) 
-			return true;
-		
-		return false;
+		return matrizEstacionesDistancia.agregarAristaMatriz(origen, destino, distancia);
 	}
 	
-	public boolean eliminarRutaDistancia(String origen, String destino) {
-		boolean a, b;
-		a = matrizEstacionesDistancia.eliminarAristaMatriz(origen, destino);
-		b = listaEstacionesDistancia.eliminarArista(origen, destino);
+	public String calcularRutaMenor(String origen, String destino) {
+		String msg="";
+		Vertice<String> src=buscarEstacionLED(origen);
+		Vertice<String> dst=buscarEstacionLED(destino);
 		
-		if (a && b)
-			return true;
+		if(dst!=null && src!=null) {
+			listaEstacionesDistancia.recorridoBFS(src);
+			if(dst.getColor().equals(Color.BLANCO))
+				return "No se puede acceder a esta estación desde este punto de origen";
+			
+			src=listaEstacionesDistancia.dijkstra(src);
+		}
+			
+		else
+			return "No existe una estación de origen con el nombre dado y/o una esteción de destino con este nombre";
+	
+		
+		boolean conti=true;
+		while(src!=null && conti) {
+			if(src.getPadre()!=null)
+				msg+=src.getDato()+" <==";
+			else
+				msg+=src.getDato();
+			
+			if(src.equals(dst))
+				conti=false;
+			
+			src=src.getPadre();
+		}
+		
+		return msg;
+		
+	}
+	
+	public String esAccesible(String origen, String destino) {
+		Vertice<String> src=buscarEstacionLED(origen);
+		Vertice<String> dst=buscarEstacionLED(destino);
+		
+		if(dst!=null && src!=null)
+			src=listaEstacionesDistancia.dijkstra(src);
+		else
+			return "No existe una estación de origen con el nombre dado y/o una esteción de destino con este nombre";
+		
+		listaEstacionesDistancia.recorridoBFS(src);
+		
+		if(dst.getColor().equals(Color.BLANCO))
+			return "No se puede acceder a esta estación desde este punto de origen";
+		else
+			return "Si hay acceso desde este punto de origen al destino";
+					
+	}
 
-		return false;
-	}
-	
 }
--- src/ui/Programa.java
+++ src/ui/Programa.java
@@ -28,10 +28,10 @@ public class Programa {
 				System.out.println("1) Agregar una estación.");
 				System.out.println("2) Eliminar una estación.");
 				System.out.println("3) Comprobar si una estación existe.");
-				System.out.println("4) Agregar una ruta con tiempo especifico.");
+				System.out.println("4) Calcular la ruta con menor coste");
 				System.out.println("5) Agregar una ruta con una distancia especifica.");
 				System.out.println("6) Eliminar una ruta que contiene distancia.");
-				System.out.println("7) Eliminar una ruta que contiene tiempo.");
+				System.out.println("7) Consultar accesibilidad de un punto a otro");
 				System.out.println("8) Salir");
 
 				String input = lector.readLine();
@@ -51,12 +51,11 @@ public class Programa {
 					opcion4();
 					break;
 				case 5:
-					opcion5();
 					break;
 				case 6:
-					opcion6();
 					break;
 				case 7:
+					opcion7();
 					break;
 				case 8:
 					terminado = true;
@@ -68,8 +67,7 @@ public class Programa {
 					System.out.println();
 				}
 			} catch (IOException e) {
-				System.out.println("Error en el flujo de entrada de datos.");
-				System.out.println("");
+
 			} catch (NumberFormatException e) {
 				System.out.println("No se permiten caracteres para las opciones disponibles.");
 				System.out.println();
@@ -86,6 +84,7 @@ public class Programa {
 			System.out.println();
 			System.out.println("¡La estación fue agregada de manera exitosa!");
 			System.out.println();
+			lector.close();
 		} catch (IOException e) {
 			System.out.println("Error en el flujo de entrada de datos.");
 		}
@@ -97,9 +96,11 @@ public class Programa {
 		try {
 			String nombreEstacion = lector.readLine();
 			boolean a = sistemaMIO.eliminarEstacionLED(nombreEstacion);
+			boolean b = sistemaMIO.eliminarEstacionLET(nombreEstacion);
 			boolean c = sistemaMIO.eliminarEstacionMED(nombreEstacion);
+			boolean d = sistemaMIO.eliminarEstacionMET(nombreEstacion);
 
-			if (a && c) {
+			if (a && b && c && d) {
 				System.out.println();
 				System.out.println("La estación ha sido eliminada.");
 				System.out.println();
@@ -119,9 +120,11 @@ public class Programa {
 		try {
 			String nombreEstacion = lector.readLine();
 			Vertice<String> a = sistemaMIO.buscarEstacionLED(nombreEstacion);
+			Vertice<String> b = sistemaMIO.buscarEstacionLET(nombreEstacion);
 			Vertice<String> c = sistemaMIO.buscarEstacionMED(nombreEstacion);
+			Vertice<String> d = sistemaMIO.buscarEstacionMET(nombreEstacion);
 
-			if (a != null && c != null) {
+			if (a != null && b != null && c != null && d != null) {
 				System.out.println();
 				System.out.println("¡Estación encontrada!");
 				System.out.println("Nombre de la estación: " + a.getDato());
@@ -147,9 +150,6 @@ public class Programa {
 				String origen = lector.readLine();
 				System.out.print("Escribe el nombre de la estación destino: ");
 				String destino = lector.readLine();
-				System.out.print("Digita el valor en minutos de la estación origen a la estación destino: ");
-				String tiempo = lector.readLine();
-				int t = Integer.parseInt(tiempo);
 
 				if (origen.equals(destino)) {
 					terminado = true;
@@ -157,99 +157,39 @@ public class Programa {
 					System.out.println("No se permiten estaciones de origen y destino con el mismo valor.");
 					System.out.println();
 				} else {
-					/*
-					if (agregado) {
-						System.out.println();
-						System.out.println("La ruta con tiempo: " + t);
-						System.out.println("Con su estación de origen: " + origen);
-						System.out.println("Y con su estación destino: " + destino);
-						System.out.println("¡Ha sido agregada de manera exitosa!");
-						System.out.println();
-					} else {
-						System.out.println();
-						System.out.println("No se pudo agregar la ruta porque no se encontraron las estaciones");
-						System.out.println();
-					}
+					System.out.println(sistemaMIO.calcularRutaMenor(origen,destino));
 				}
 
 				terminado = true;
+				lector.close();
 			} catch (IOException e) {
 				System.out.println("Error en el flujo de entrada de datos.");
 				terminado = true;
-			} catch (NumberFormatException e) {
-				System.out.println();
-				System.out.println("No se permiten caracteres para el tiempo de la ruta.");
-				System.out.println("Por favor, intente nuevamente.");
-				System.out.println();
 			}
 		}
 	}
 
-	private void opcion5() {
-		// Recuerda: Distancia.
-		BufferedReader lector = new BufferedReader(new InputStreamReader(System.in));
-		boolean terminado = false;
-
-		while (!terminado) {
-			try {
-				System.out.print("Escribe el nombre de la estación origen: ");
-				String origen = lector.readLine();
-				System.out.print("Escribe el nombre de la estación destino: ");
-				String destino = lector.readLine();
-				System.out.print("Digita el valor en metros de la estación origen a la estación destino: ");
-				String distancia = lector.readLine();
-				int d = Integer.parseInt(distancia);
-
-				if (origen.equals(destino)) {
-					terminado = true;
-					System.out.println();
-					System.out.println("No se permiten estaciones de origen y destino con el mismo valor.");
-					System.out.println();
-				} else {
-					boolean agregado = sistemaMIO.agregarRutaDistancia(origen, destino, d);
-					if (agregado) {
-						System.out.println();
-						System.out.println("La ruta con distancia: " + d);
-						System.out.println("Con su estación de origen: " + origen);
-						System.out.println("Y con su estación destino: " + destino);
-						System.out.println("¡Ha sido agregada de manera exitosa!");
-						System.out.println();
-					} else {
-						System.out.println();
-						System.out.println("No se pudo agregar la ruta porque no se encontraron las estaciones");
-						System.out.println();
-					}
-				}
-
-				terminado = true;
-			} catch (IOException e) {
-				System.out.println("Error en el flujo de entrada de datos.");
-				terminado = true;
-			} catch (NumberFormatException e) {
-				System.out.println();
-				System.out.println("No se permiten caracteres para la distancia de la ruta.");
-				System.out.println("Por favor, intente nuevamente.");
-				System.out.println();
-			}
-		}
-	}
-
-	private void opcion6() {
-		// Recuerda: Distancia.
+	public void opcion7() {
 		BufferedReader lector = new BufferedReader(new InputStreamReader(System.in));
 		try {
+			System.out.println("Escribe el nombre de la estación origen: ");
 			String origen = lector.readLine();
+			System.out.print("Escribe el nombre de la estación destino: ");
 			String destino = lector.readLine();
-			
+
 			if (origen.equals(destino)) {
 				System.out.println();
 				System.out.println("No se permiten estaciones de origen y destino con el mismo valor.");
 				System.out.println();
 			} else {
-				boolean eliminado = sistemaMIO.eliminarRutaDistancia(origen, destino);
+				System.out.println();
+				System.out.println(sistemaMIO.esAccesible(origen,destino));
+				System.out.println();
 			}
+
+			lector.close();
 		} catch (IOException e) {
-			
+			System.out.println("Error en el flujo de entrada de datos.");
 		}
 	}
 
